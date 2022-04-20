048aed5408b17557e31d1bc8dd0793a82a1c4b7c
pany <939630029@qq.com>
2022-04-21 00:50:12 +0800

    chore: 路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { ConfigEnv, UserConfigExport } from "vite"
+import { resolve } from "path"
+import vue from "@vitejs/plugin-vue"
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+/** 配置项文档：https://vitejs.dev/config */
+export default (env: ConfigEnv): UserConfigExport => {
+  return {
+    resolve: {
+      alias: {
+        /** @ 符号指向 src 目录 */
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+    plugins: [vue()],
+  }
+}
