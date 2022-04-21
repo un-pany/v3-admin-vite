1c46b05d07048052812ddcf937504d8640d6ae07
pany <939630029@qq.com>
2022-04-21 18:54:44 +0800

    docs: readme & 链接

--- src/layout/components/NavigationBar/index.vue
+++ src/layout/components/NavigationBar/index.vue
@@ -10,17 +10,20 @@
         <el-avatar :icon="UserFilled" :size="34" />
         <template #dropdown>
           <el-dropdown-menu>
+            <a target="_blank" href="https://github.com/un-pany/v3-admin-vite">
+              <el-dropdown-item>V3-Admin-Vite GitHub</el-dropdown-item>
+            </a>
             <a target="_blank" href="https://juejin.cn/post/6963876125428678693">
-              <el-dropdown-item>中文文档</el-dropdown-item>
+              <el-dropdown-item>V3-Admin 中文文档</el-dropdown-item>
             </a>
             <a target="_blank" href="https://github.com/un-pany/v3-admin/blob/master/README.en.md">
-              <el-dropdown-item>English Docs</el-dropdown-item>
+              <el-dropdown-item>V3-Admin English Docs</el-dropdown-item>
             </a>
             <a target="_blank" href="https://github.com/un-pany/v3-admin">
-              <el-dropdown-item>GitHub</el-dropdown-item>
+              <el-dropdown-item>V3-Admin GitHub</el-dropdown-item>
             </a>
             <a target="_blank" href="https://gitee.com/un-pany/v3-admin">
-              <el-dropdown-item>Gitee</el-dropdown-item>
+              <el-dropdown-item>V3-Admin Gitee</el-dropdown-item>
             </a>
             <el-dropdown-item divided @click="state.logout">
               <span style="display: block">退出登录</span>
