9b4af24d41677c70f450eb58260ed733971c9034
pany <939630029@qq.com>
2022-04-21 17:35:30 +0800

    chore: 新增 prettier 忽略文件

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -5,7 +5,11 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ElButton: typeof import('element-plus/es')['ElButton']
     HelloWorld: typeof import('./../components/HelloWorld.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+    SvgIcon: typeof import('./../components/SvgIcon/index.vue')['default']
   }
 }
 
