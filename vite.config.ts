0f7092151a72ad03b4ae5a3286e1b4bcff921d2f
pany <939630029@qq.com>
2022-08-19 23:01:07 +0800

    fix: 环境变量 VITE_PUBLIC_PATH 没有生效的问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { UserConfigExport } from "vite"
+import { UserConfigExport, loadEnv } from "vite"
+import type { ConfigEnv } from "vite"
 import path, { resolve } from "path"
 import vue from "@vitejs/plugin-vue"
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons"
 import UnoCSS from "unocss/vite"
 
 /** 配置项文档：https://cn.vitejs.dev/config */
-export default (): UserConfigExport => {
+export default (configEnv: ConfigEnv): UserConfigExport => {
+  const viteEnv = loadEnv(configEnv.mode, process.cwd()) as ImportMetaEnv
+  const { VITE_PUBLIC_PATH } = viteEnv
   return {
     /** 打包时根据实际情况修改 base */
-    base: import.meta.env.VITE_PUBLIC_PATH,
+    base: VITE_PUBLIC_PATH,
     resolve: {
       alias: {
         /** @ 符号指向 src 目录 */
