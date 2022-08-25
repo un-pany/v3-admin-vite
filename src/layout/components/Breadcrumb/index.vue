1d954025fb4816b95014ef2542d12ec182c34391
pany <939630029@qq.com>
2022-08-25 17:11:34 +0800

    refactor: 移除 breadcrumb transition

--- src/layout/components/Breadcrumb/index.vue
+++ src/layout/components/Breadcrumb/index.vue
@@ -48,16 +48,14 @@ getBreadcrumb()
 
 <template>
   <el-breadcrumb class="app-breadcrumb">
-    <transition-group name="breadcrumb">
-      <el-breadcrumb-item v-for="(item, index) in breadcrumbs" :key="item.path">
-        <span v-if="item.redirect === 'noRedirect' || index === breadcrumbs.length - 1" class="no-redirect">
-          {{ item.meta.title }}
-        </span>
-        <a v-else @click.prevent="handleLink(item)">
-          {{ item.meta.title }}
-        </a>
-      </el-breadcrumb-item>
-    </transition-group>
+    <el-breadcrumb-item v-for="(item, index) in breadcrumbs" :key="item.path">
+      <span v-if="item.redirect === 'noRedirect' || index === breadcrumbs.length - 1" class="no-redirect">
+        {{ item.meta.title }}
+      </span>
+      <a v-else @click.prevent="handleLink(item)">
+        {{ item.meta.title }}
+      </a>
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
