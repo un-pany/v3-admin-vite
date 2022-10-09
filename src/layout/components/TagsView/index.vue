fa29e1d36484783417db96d98d99a12e4e4e1e7b
pany <939630029@qq.com>
2022-10-09 22:56:13 +0800

    perf: TagsView

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -205,10 +205,10 @@ onMounted(() => {
       margin-left: 5px;
       margin-top: 4px;
       &:first-of-type {
-        margin-left: 24px;
+        margin-left: 5px;
       }
       &:last-of-type {
-        margin-right: 24px;
+        margin-right: 5px;
       }
       &.active {
         background-color: var(--v3-tagsview-tag-active-bg-color);
