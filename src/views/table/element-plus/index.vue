4af9112121a25198936fa5ea67213ed84d7bea34
pany <939630029@qq.com>
2022-10-24 11:02:23 +0800

    style: 更改深蓝色主题样式风格

--- src/views/table/element-plus/index.vue
+++ src/views/table/element-plus/index.vue
@@ -155,10 +155,10 @@ getTableData()
           <el-button type="danger" :icon="Delete">批量删除</el-button>
         </div>
         <div>
-          <el-tooltip content="下载" effect="light">
+          <el-tooltip content="下载">
             <el-button type="primary" :icon="Download" circle />
           </el-tooltip>
-          <el-tooltip content="刷新表格" effect="light">
+          <el-tooltip content="刷新表格">
             <el-button type="primary" :icon="RefreshRight" circle @click="handRefresh" />
           </el-tooltip>
         </div>
