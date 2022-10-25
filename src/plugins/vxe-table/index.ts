22c95b940f6330efae1a7e8bdb1d47c28bd79069
pany <939630029@qq.com>
2022-10-25 10:45:26 +0800

    style: 格式化 vxe-table 全局默认参数

--- src/plugins/vxe-table/index.ts
+++ src/plugins/vxe-table/index.ts
@@ -5,31 +5,42 @@ import VXETable from "vxe-table"
 import VXETablePluginElement from "vxe-table-plugin-element"
 
 VXETable.use(VXETablePluginElement)
-// 全局默认参数
+
+/** 全局默认参数 */
 VXETable.setup({
-  // size: "medium", // 全局尺寸
-  zIndex: 1000, // 全局 zIndex 起始值，如果项目的的 z-index 样式值过大时就需要跟随设置更大，避免被遮挡
-  version: 0, // 版本号，对于某些带数据缓存的功能有用到，上升版本号可以用于重置数据
-  loadingText: null, // 全局loading提示内容，如果为 null 则不显示文本
+  /** 全局尺寸 */
+  // size: "medium",
+  /** 全局 zIndex 起始值，如果项目的的 z-index 样式值过大时就需要跟随设置更大，避免被遮挡 */
+  zIndex: 9999,
+  /** 版本号，对于某些带数据缓存的功能有用到，上升版本号可以用于重置数据 */
+  version: 0,
+  /** 全局 loading 提示内容，如果为 null 则不显示文本 */
+  loadingText: null,
   table: {
-    align: "center",
-    border: "inner",
-    headerAlign: "center",
     showHeader: true,
     showOverflow: "tooltip",
     showHeaderOverflow: "tooltip",
     autoResize: true,
-    //   stripe: false,
-    //   border: false,
-    //   round: false,
+    // stripe: false,
+    border: "inner",
+    // round: false,
     emptyText: "暂无数据",
-    rowId: "_XID", // 行数据的唯一主键字段名
-    rowConfig: { isHover: true, isCurrent: true },
-    columnConfig: { resizable: true }
+    rowConfig: {
+      isHover: true,
+      isCurrent: true
+    },
+    columnConfig: {
+      resizable: true
+    },
+    align: "center",
+    headerAlign: "center",
+    /** 行数据的唯一主键字段名 */
+    rowId: "_XID"
   },
   pager: {
-    // size: 'small',
-    perfect: false, // 配套的样式
+    // size: "small",
+    /** 配套的样式 */
+    perfect: false,
     pageSize: 10,
     pagerCount: 7,
     pageSizes: [5, 10, 20, 50, 100, 150],
@@ -38,14 +49,14 @@ VXETable.setup({
   modal: {
     minWidth: 500,
     minHeight: 400,
-    mask: true,
     lockView: true,
-    transfer: true,
-    draggable: false,
+    mask: true,
     // duration: 3000,
     // marginSize: 20,
     dblclickZoom: false,
-    showTitleOverflow: true
+    showTitleOverflow: true,
+    transfer: true,
+    draggable: false
   }
 })
 
