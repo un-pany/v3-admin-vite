8389a52af96157c25c7e92bcb8bf054735bd7e06
pany <939630029@qq.com>
2022-11-07 17:46:28 +0800

    mod: 优化首页和项目外链

--- src/views/dashboard/editor/index.vue
+++ src/views/dashboard/editor/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <div class="app-container">Editor 权限可见</div>
+  <div class="app-container center">
+    <el-empty description="Editor 权限可见" />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.center {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
