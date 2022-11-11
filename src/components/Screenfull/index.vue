d17bbc8b094557b223dcea12691f0c3317ee4418
Sunny-117 <73089592+Sunny-117@users.noreply.github.com>
2022-11-11 23:06:15 +0800

    fix: remove defineProps import

--- src/components/Screenfull/index.vue
+++ src/components/Screenfull/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref, onUnmounted, defineProps } from "vue"
+import { ref, onUnmounted } from "vue"
 import { ElMessage } from "element-plus"
 import screenfull from "screenfull"
 
