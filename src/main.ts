4d197b42247829aa27eb0e6a9c8993d705688115
付志强 <zhiqiangfu6@gmail.com>
2022-11-12 11:44:22 +0800

    feat: add watermark

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,34 @@ import "element-plus/theme-chalk/dark/css-vars.css"
 import "vxe-table/lib/style.css"
 import "vxe-table-plugin-element/dist/style.css"
 import "@/styles/index.scss"
+import { Watermark } from '@pansy/watermark';
 
+/*****************************************水印区域，开发者自行决定是否添加水印****************************************** */
+
+const addWaterMark = () => {
+  const watermark = new Watermark({
+    text: 'v3-admin-vite'
+  });
+
+  // 如果需要修改水印参数，请调用
+
+  // watermark.update({
+
+  // });
+
+  // 隐藏水印
+  // watermark.hide();
+
+  // 显示水印
+  watermark.show();
+
+  // 销毁水印
+  // watermark.destroy();
+}
+
+addWaterMark()
+
+/*****************************************水印区域，开发者自行决定是否添加水印****************************************** */
 const app = createApp(App)
 
 /** 加载插件 */
