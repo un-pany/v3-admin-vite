e31b9674d21a2175abc7264fde812d2483d0350d
ClariS <1457715339@qq.com>
2023-01-17 10:26:47 +0800

    feat: 新增 Select V2 示例

--- src/views/hooks/use-fetch-select.vue
+++ src/views/hooks/use-fetch-select.vue
@@ -12,5 +12,7 @@ const { loading, options, selectedValue } = useFetchSelect({
     <el-select :loading="loading" v-model="selectedValue">
       <el-option v-for="(item, index) in options" v-bind="item" :key="index" />
     </el-select>
+    <span class="m-x">Select V2 示例</span>
+    <el-select-v2 :loading="loading" v-model="selectedValue" :options="options" />
   </div>
 </template>
