ebfa35d92f30f04a1b09fe2d9844c1b1ab5acae0
pany <939630029@qq.com>
2023-02-28 14:11:39 +0800

    refactor: 更改 "欢迎 star 标语" 代码位置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
+import { h } from "vue"
 import { useTheme } from "@/hooks/useTheme"
+import { ElNotification } from "element-plus"
 import zhCn from "element-plus/lib/locale/lang/zh-cn"
 
 const { initTheme } = useTheme()
@@ -8,6 +10,17 @@ const { initTheme } = useTheme()
 initTheme()
 /** 将 Element Plus 的语言设置为中文 */
 const locale = zhCn
+
+ElNotification({
+  title: "Hello",
+  message: h(
+    "a",
+    { style: "color: teal", target: "_blank", href: "https://github.com/un-pany/v3-admin-vite" },
+    "小项目获取 star 不易，如果你喜欢这个项目的话，欢迎点击这里支持一个 star ！这是作者持续维护的唯一动力（小声：毕竟是免费的）"
+  ),
+  duration: 0,
+  position: "bottom-right"
+})
 </script>
 
 <template>
