dd2f24cec5a68d8e9cacd0cc0b3d26e9e9c48c60
pany <939630029@qq.com>
2023-04-21 18:32:32 +0800

    revert: 撤销不属于该 PR 的代码改动

--- src/plugins/element-plus/index.ts
+++ src/plugins/element-plus/index.ts
@@ -1,7 +1,5 @@
 import { type App } from "vue"
 import ElementPlus from "element-plus"
-import "element-plus/dist/index.css"
-import "element-plus/theme-chalk/dark/css-vars.css"
 
 export function loadElementPlus(app: App) {
   /** Element Plus 组件完整引入 */
