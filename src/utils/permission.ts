2ec928434bb69dfaf93e7afbc6d6562ad61d9cda
pany <939630029@qq.com>
2023-04-26 16:51:11 +0800

    docs: 修正权限函数报错提示文案

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -9,7 +9,7 @@ export const checkPermission = (value: string[]): boolean => {
       return permissionRoles.includes(role)
     })
   } else {
-    console.error("need roles! Like v-permission=\"['admin','editor']\"")
+    console.error("need roles! Like checkPermission(['admin','editor'])")
     return false
   }
 }
