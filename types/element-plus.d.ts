362ce6ed566270ddd07acfc40dcc1d05fe750e99
pany <939630029@qq.com>
2023-05-12 18:59:39 +0800

    docs: 优化 element-plus.d.ts 文件注释

--- types/element-plus.d.ts
+++ types/element-plus.d.ts
@@ -1,4 +1,7 @@
-/** Copy https://github.com/element-plus/element-plus/blob/dev/global.d.ts#L2 */
+/**
+ * Copy https://github.com/element-plus/element-plus/blob/dev/global.d.ts
+ * 为了解决某些依赖（比如 vxe-table 4.2.7-beta.0 ~ 4.3.11 和 vue-router 4.2.0）导致的没有 Element Plus 组件类型提示的问题
+ */
 
 declare module 'vue' {
   export interface GlobalComponents {
