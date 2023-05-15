f28ae2a68bf74f5da363ba2c0e1bad658855a4b8
pany-ang <939630029@qq.com>
2023-05-15 06:54:27 +0000

    Deploying to gh-pages from  @ b69abd8e1a9fd455a81674aface84b91190d235b 🚀

--- static/index-01d9ac9e.js
+++
@@ -1,1 +0,0 @@
-import{ao as t}from"./index-e4fff7a4.js";function e(e){return t({url:"table",method:"post",data:e})}function a(e){return t({url:`table/${e}`,method:"delete"})}function r(e){return t({url:"table",method:"put",data:e})}function u(e){return t({url:"table",method:"get",params:e})}export{e as c,a as d,u as g,r as u};
