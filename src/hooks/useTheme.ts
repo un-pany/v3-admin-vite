63844a8f2e793b87be3692f257b412befc26ef31
pany <939630029@qq.com>
2023-05-19 21:02:20 +0800

    perf: 代码优化 hooks/useTheme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -31,19 +31,22 @@ const themeList: IThemeList[] = [
 /** 正在应用的主题名称 */
 const activeThemeName = ref<ThemeName>(getActiveThemeName() || DEFAULT_THEME_NAME)
 
+/** 设置主题 */
 const setTheme = (value: ThemeName) => {
   activeThemeName.value = value
 }
 
 /** 在 html 根元素上挂载 class */
-const setHtmlClassName = (value: ThemeName) => {
+const setHtmlRootClassName = (value: ThemeName) => {
   document.documentElement.className = value
 }
 
+/** 初始化 */
 const initTheme = () => {
+  // watchEffect 来收集副作用
   watchEffect(() => {
     const value = activeThemeName.value
-    setHtmlClassName(value)
+    setHtmlRootClassName(value)
     setActiveThemeName(value)
   })
 }
