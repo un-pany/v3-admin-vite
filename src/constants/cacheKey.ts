17dc453350776794dd3393dd6739a4adec4ba553
pany <939630029@qq.com>
2023-05-21 11:49:03 +0800

    perf: 代码优化 constants/cacheKey

--- src/constants/cacheKey.ts
+++ src/constants/cacheKey.ts
@@ -2,9 +2,9 @@ const SYSTEM_NAME = "v3-admin-vite"
 
 /** 缓存数据时用到的 Key */
 class CacheKey {
-  static TOKEN = `${SYSTEM_NAME}-token-key`
-  static SIDEBAR_STATUS = `${SYSTEM_NAME}-sidebar-status-key`
-  static ACTIVE_THEME_NAME = `${SYSTEM_NAME}-active-theme-name-key`
+  static readonly TOKEN = `${SYSTEM_NAME}-token-key`
+  static readonly SIDEBAR_STATUS = `${SYSTEM_NAME}-sidebar-status-key`
+  static readonly ACTIVE_THEME_NAME = `${SYSTEM_NAME}-active-theme-name-key`
 }
 
 export default CacheKey
