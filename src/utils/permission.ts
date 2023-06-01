4c08e5f1908c90dc55fd5b2e2d0683c003fa1aa2
pany <939630029@qq.com>
2023-06-01 18:00:08 +0800

    perf: 代码优化 utils/permission 和 directives/permission

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -1,13 +1,10 @@
 import { useUserStoreHook } from "@/store/modules/user"
 
-/** 全局权限判断函数，和指令 v-permission 功能类似 */
-export const checkPermission = (value: string[]): boolean => {
-  if (value && value instanceof Array && value.length > 0) {
-    const roles = useUserStoreHook().roles
-    const permissionRoles = value
-    return roles.some((role) => {
-      return permissionRoles.includes(role)
-    })
+/** 全局权限判断函数，和权限指令 v-permission 功能类似 */
+export const checkPermission = (permissionRoles: string[]): boolean => {
+  if (Array.isArray(permissionRoles) && permissionRoles.length > 0) {
+    const { roles } = useUserStoreHook()
+    return roles.some((role) => permissionRoles.includes(role))
   } else {
     console.error("need roles! Like checkPermission(['admin','editor'])")
     return false
