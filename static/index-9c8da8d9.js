a9ba3454604c793aa7cd93befd550e356fdd2eeb
pany-ang <939630029@qq.com>
2023-06-29 10:01:55 +0000

    Deploying to gh-pages from  @ 665bdbec7a055817aaf0ebe67abd49ad95a7ab43 🚀

---
+++ static/index-9c8da8d9.js
@@ -0,0 +1,1 @@
+import{b as a,u as e,p as s,h as r,o as t}from"./index-1db4ca6c.js";const p=a({__name:"index",setup(a){const p=e();return s().replace({path:"/"+p.params.path,query:p.query}),(a,e)=>(t(),r("div"))}});export{p as default};
