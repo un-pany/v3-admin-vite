d2a05a825fe593300b6e7517deb9c84a569ee27b
pany <939630029@qq.com>
2023-08-07 12:02:09 +0800

    perf: 删除冗余逻辑

--- src/utils/routerHelper.ts
+++ src/utils/routerHelper.ts
@@ -22,7 +22,7 @@ export const flatMultiLevelRoutes = (routes: RouteRecordRaw[]) => {
 const isMultipleRoute = (route: RouteRecordRaw) => {
   const children = route.children
   if (children?.length) {
-    // 只要有一个子路由的 children 长度大于 0，就说明是三级以上路由
+    // 只要有一个子路由的 children 长度大于 0，就说明是三级及其以上路由
     return children.some((child) => child.children?.length)
   }
   return false
@@ -48,22 +48,19 @@ const promoteRouteLevel = (route: RouteRecordRaw) => {
 
 /** 将给定的子路由添加到指定的路由模块中 */
 const addToChildren = (routes: RouteRecordNormalized[], children: RouteRecordRaw[], routeModule: RouteRecordRaw) => {
-  children
-    // 过滤出在 routes 数组中有匹配名称的子路由
-    .filter((child) => routes.some((item) => item.name === child.name))
-    .forEach((child) => {
-      const route = routes.find((item) => item.name === child.name)
-      if (route) {
-        // 初始化 routeModule 的 children
-        routeModule.children = routeModule.children || []
-        // 如果 routeModule 的 children 属性中不包含该路由，则将其添加进去
-        if (!routeModule.children.includes(route)) {
-          routeModule.children.push(route)
-        }
-        // 如果该子路由还有自己的子路由，则递归调用此函数将它们也添加进去
-        if (child.children?.length) {
-          addToChildren(routes, child.children, routeModule)
-        }
+  children.forEach((child) => {
+    const route = routes.find((item) => item.name === child.name)
+    if (route) {
+      // 初始化 routeModule 的 children
+      routeModule.children = routeModule.children || []
+      // 如果 routeModule 的 children 属性中不包含该路由，则将其添加进去
+      if (!routeModule.children.includes(route)) {
+        routeModule.children.push(route)
       }
-    })
+      // 如果该子路由还有自己的子路由，则递归调用此函数将它们也添加进去
+      if (child.children?.length) {
+        addToChildren(routes, child.children, routeModule)
+      }
+    }
+  })
 }
