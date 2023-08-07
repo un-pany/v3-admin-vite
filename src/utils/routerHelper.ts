31e71dc549e436fb3d7a2338c96bf72a4f4a5d23
pany <939630029@qq.com>
2023-08-07 11:48:34 +0800

    perf: 优化代码

--- src/utils/routerHelper.ts
+++ src/utils/routerHelper.ts
@@ -1,18 +1,18 @@
-import { cloneDeep, omit } from "lodash-es"
 import {
-  RouteRecordNormalized,
-  RouteRecordRaw,
-  Router,
+  type Router,
+  type RouteRecordNormalized,
+  type RouteRecordRaw,
   createRouter,
   createWebHashHistory,
   createWebHistory
 } from "vue-router"
+import { cloneDeep, omit } from "lodash-es"
 
-/** 路由降级，把三级以上的路由转化为二级路由 */
+/** 路由降级（把三级及其以上的路由转化为二级路由） */
 export const flatMultiLevelRoutes = (routes: RouteRecordRaw[]) => {
   const routesMirror = cloneDeep(routes)
   routesMirror.forEach((route) => {
-    // 如果路由是三级以上路由，对其进行降级处理
+    // 如果路由是三级及其以上路由，对其进行降级处理
     isMultipleRoute(route) && promoteRouteLevel(route)
   })
   return routesMirror
@@ -20,20 +20,23 @@ export const flatMultiLevelRoutes = (routes: RouteRecordRaw[]) => {
 
 /** 判断路由层级是否大于 2 */
 const isMultipleRoute = (route: RouteRecordRaw) => {
-  if (!route || !Reflect.has(route, "children") || !route.children?.length) return false
-  // 只要有一个子路由的 children 长度大于 0，就说明是三级以上路由
-  return route.children.some((child) => child.children?.length)
+  const children = route.children
+  if (children?.length) {
+    // 只要有一个子路由的 children 长度大于 0，就说明是三级以上路由
+    return children.some((child) => child.children?.length)
+  }
+  return false
 }
 
 /** 生成二级路由 */
 const promoteRouteLevel = (route: RouteRecordRaw) => {
   // 创建 router 实例是为了获取到当前传入的 route 的所有路由信息
   let router: Router | null = createRouter({
-    routes: [route],
     history:
       import.meta.env.VITE_ROUTER_HISTORY === "hash"
         ? createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH)
-        : createWebHistory(import.meta.env.VITE_PUBLIC_PATH)
+        : createWebHistory(import.meta.env.VITE_PUBLIC_PATH),
+    routes: [route]
   })
   const routes = router.getRoutes()
   // 在 addToChildren 函数中使用上面获取到的路由信息来更新 route 的 children
