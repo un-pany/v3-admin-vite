0d8c69cfff3a633791953547d64fce52a5daf4f7
pany-ang <939630029@qq.com>
2023-08-21 09:24:19 +0000

    Deploying to gh-pages from  @ 82c3409b7df260f3723bd2c690ccc90165c74986 🚀

--- static/index-c17a2a42.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,i as e,j as s,k as n,m as r,n as t}from"./index-2b7d68f9.js";const o={class:"app-container"};const c=a({},[["render",function(a,c){const d=e("router-view"),i=e("el-card");return s(),n("div",o,[r(i,{header:"二级路由 - menu1"},{default:t((()=>[r(d)])),_:1})])}]]);export{c as default};
