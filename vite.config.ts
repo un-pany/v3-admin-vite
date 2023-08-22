341e3cadee31bceb1e8c6a7893d0eb4df8af8792
张浩杰 <havoc_zhang@yeah.net>
2023-08-22 20:16:14 +0800

    perf: 增加打包分块修改混淆编译为esbuild

--- vite.config.ts
+++ vite.config.ts
@@ -48,22 +48,20 @@ export default (configEnv: ConfigEnv): UserConfigExport => {
     build: {
       /** 消除打包大小超过 500kb 警告 */
       chunkSizeWarningLimit: 2000,
-      /** Vite 2.6.x 以上需要配置 minify: "terser", terserOptions 才能生效 */
-      minify: "terser",
-      /** 在打包代码时移除 console.log、debugger 和 注释 */
-      terserOptions: {
-        compress: {
-          drop_console: false,
-          drop_debugger: true,
-          pure_funcs: ["console.log"]
-        },
-        format: {
-          /** 删除注释 */
-          comments: false
-        }
-      },
       /** 打包后静态资源目录 */
-      assetsDir: "static"
+      assetsDir: "static",
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vue: ["vue", "vue-router", "pinia"],
+            utils: ["lodash-es", "js-cookie", "screenfull", "path-browserify", "path-to-regexp"],
+            vxe: ["vxe-table", "vxe-table-plugin-element", "xe-utils"]
+          }
+        }
+      }
+    },
+    esbuild: {
+      drop: ["console", "debugger"]
     },
     /** Vite 插件 */
     plugins: [
