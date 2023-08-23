868edb6b2458ca38b85d4752f89238e6a949710e
张浩杰 <havoc_zhang@yeah.net>
2023-08-23 21:08:55 +0800

    fix: 修复问题(#114)

--- src/layouts/components/AppMain.vue
+++ src/layouts/components/AppMain.vue
@@ -1,27 +1,22 @@
 <script lang="ts" setup>
-import { computed } from "vue"
-import { useRoute } from "vue-router"
 import { useTagsViewStore } from "@/store/modules/tags-view"
 
-const route = useRoute()
 const tagsViewStore = useTagsViewStore()
-
-const key = computed(() => {
-  // 返回 route.path 和 route.fullPath 有着不同的效果，大多数时候 path 更通用
-  return route.path
-})
 </script>
 
 <template>
   <section class="app-main">
     <div class="app-scrollbar">
-      <router-view v-slot="{ Component }">
-        <transition name="el-fade-in" mode="out-in">
-          <keep-alive :include="tagsViewStore.cachedViews">
-            <component :is="Component" :key="key" />
-          </keep-alive>
-        </transition>
-      </router-view>
+      <RouterView>
+        <template #default="{ Component, route }">
+          <transition name="fade" mode="out-in" appear>
+            <keep-alive v-if="route.meta.keepAlive" :include="tagsViewStore.cachedViews">
+              <component :is="Component" :key="route.fullPath" />
+            </keep-alive>
+            <component v-else :is="Component" :key="route.fullPath" />
+          </transition>
+        </template>
+      </RouterView>
     </div>
     <!-- 返回顶部 -->
     <el-backtop />
