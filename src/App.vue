36f8c8acf2ae82e75fb1b04e55d72d0afe4848a2
pany <939630029@qq.com>
2023-08-28 16:52:17 +0800

    docs: 优化一些文案

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import { h } from "vue"
 import { useTheme } from "@/hooks/useTheme"
-import { resetConfigLayout } from "@/utils"
 import { ElNotification } from "element-plus"
 // 将 Element Plus 的语言设置为中文
 import zhCn from "element-plus/es/locale/lang/zh-cn"
@@ -22,21 +21,6 @@ ElNotification({
   ),
   duration: 0,
   position: "bottom-right"
-})
-ElNotification({
-  title: "破坏性更新",
-  type: "warning",
-  message: h(
-    "span",
-    {
-      style: "color: teal; cursor: pointer;",
-      onClick: resetConfigLayout
-    },
-    "由于平台 4.1.0 版本新增了多种布局模式，如果第一次出现白屏情况，点击这里或手动前往控制台清理 LocalStorage 缓存数据后刷新页面即可！"
-  ),
-  duration: 0,
-  position: "bottom-right",
-  offset: 150
 })
 </script>
 
--- src/views/hook-demo/use-fullscreen-loading.vue
+++ src/views/hook-demo/use-fullscreen-loading.vue
@@ -26,7 +26,7 @@ const querySuccess = async () => {
   // 1. getSuccessApi 是一个函数而非函数调用
   // 2. 如需给 getSuccessApi 函数传递参数，请在后面的括号中进行（真正的 getSuccessApi 调用）
   const res = await useFullscreenLoading(getSuccessApi)([2, 3, 3])
-  ElMessage.success(`${res.message}，传参为${res.data.list}`)
+  ElMessage.success(`${res.message}，传参为 ${res.data.list.toString()}`)
 }
 
 const queryError = async () => {
