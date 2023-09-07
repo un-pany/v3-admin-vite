81afce0172c4334fd307476bcc50552011ac16f3
ClariS <1457715339@qq.com>
2023-09-07 17:09:04 +0800

    fix: 替换 %placeholder 后侧边栏 tip-line 样式丢失

--- src/layouts/components/Sidebar/index.vue
+++ src/layouts/components/Sidebar/index.vue
@@ -145,18 +145,14 @@ const tipLineWidth = computed(() => {
   }
 }
 
-:deep(.el-menu-item) {
-  &.is-active {
-    @extend %tip-line;
-  }
+:deep(.el-menu-item.is-active) {
+  @extend %tip-line;
 }
 
 .el-menu--collapse {
-  :deep(.el-sub-menu) {
-    &.is-active {
-      .el-sub-menu__title {
-        @extend %tip-line;
-      }
+  :deep(.el-sub-menu.is-active) {
+    .el-sub-menu__title {
+      @extend %tip-line;
     }
   }
 }
