80e7e9ea9f710d258c2f4df6cc89c16cf4504f57
pany <939630029@qq.com>
2023-09-19 10:01:21 +0800

    docs: 保留一些原有的注释

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -17,17 +17,17 @@ NProgress.configure({ showSpinner: false })
 router.beforeEach(async (to, _from, next) => {
   fixBlankPage()
   NProgress.start()
-
   const userStore = useUserStoreHook()
   const permissionStore = usePermissionStoreHook()
   const token = getToken()
 
   // 判断该用户是否已经登录
-  if (!token || token === "") {
+  if (!token) {
     // 如果在免登录的白名单中，则直接进入
     if (isWhiteList(to)) {
       next()
     } else {
+      // 其他没有访问权限的页面将被重定向到登录页面
       NProgress.done()
       next("/login")
     }
@@ -49,7 +49,6 @@ router.beforeEach(async (to, _from, next) => {
       // 注意：角色必须是一个数组！ 例如: ['admin'] 或 ['developer', 'editor']
       await userStore.getInfo()
       const roles = userStore.roles
-
       // 根据角色生成可访问的 Routes（可访问路由 = 常驻路由 + 有访问权限的动态路由）
       permissionStore.setRoutes(roles)
     } else {
@@ -57,10 +56,10 @@ router.beforeEach(async (to, _from, next) => {
       userStore.setRoles(routeSettings.defaultRoles)
       permissionStore.setRoutes(routeSettings.defaultRoles)
     }
-
     // 将'有访问权限的动态路由' 添加到 Router 中
     permissionStore.dynamicRoutes.forEach((route) => router.addRoute(route))
-
+    // 确保添加路由已完成
+    // 设置 replace: true, 因此导航将不会留下历史记录
     next({ ...to, replace: true })
   } catch (err: any) {
     // 过程中发生任何错误，都直接重置 Token，并重定向到登录页面
