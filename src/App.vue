bfd0ecb44f6146925f19a08e7d5579b95e5e5087
25hours886 <1094662341@qq.com>
2024-01-03 21:43:03 +0800

    .github
    Signed-off-by: 25hours886 <1094662341@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script lang="ts" setup>
-import { h } from "vue"
 import { useTheme } from "@/hooks/useTheme"
-import { ElNotification } from "element-plus"
 // 将 Element Plus 的语言设置为中文
 import zhCn from "element-plus/es/locale/lang/zh-cn"
 
@@ -11,17 +9,6 @@ const { initTheme } = useTheme()
 initTheme()
 
 /** 作者小心思 */
-ElNotification({
-  title: "Hello",
-  type: "success",
-  message: h(
-    "a",
-    { style: "color: teal", target: "_blank", href: "https://github.com/un-pany/v3-admin-vite" },
-    "小项目获取 star 不易，如果你喜欢这个项目的话，欢迎点击这里支持一个 star ！这是作者持续维护的唯一动力（小声：毕竟是免费的）"
-  ),
-  duration: 0,
-  position: "bottom-right"
-})
 </script>
 
 <template>
