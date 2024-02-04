89e8d81459b476ca878a3ae90df23ee705eccada
pany-ang <939630029@qq.com>
2024-02-04 09:15:43 +0000

    Deploying to gh-pages from  @ fff3eb9498ff8e453a6cc4a5d0ead894d1265f20 🚀

--- static/index-dh-x4PbC.js
+++
@@ -1,1 +0,0 @@
-import{l as t,aC as r,aD as a,m as o,p as s}from"./vue-wOuPBSxb.js";const _=t({__name:"index",setup(n){const e=r();return a().replace({path:"/"+e.params.path,query:e.query}),(p,c)=>(o(),s("div"))}});export{_ as default};
