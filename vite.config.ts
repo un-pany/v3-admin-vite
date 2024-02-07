e81668e2935c6888adfdb88bf5992745298fd3f4
pany <939630029@qq.com>
2024-02-07 10:38:12 +0800

    chore: 切换在线 mock 接口地址

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default (configEnv: ConfigEnv): UserConfigExport => {
       /** 接口代理 */
       proxy: {
         "/api/v1": {
-          target: "https://www.fastmock.site/mock/761e2dda2b8890ab86c928a74e8f6538",
+          target: "https://mock.mengxuegu.com/mock/63218b5fb4c53348ed2bc212",
           ws: true,
           /** 是否允许跨域 */
           changeOrigin: true
