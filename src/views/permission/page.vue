a00e57a1e842179f005de95e7fa57862b47ffc20
pany <939630029@qq.com>
2024-02-07 10:32:53 +0800

    perf: 移除遗留的废弃逻辑

--- src/views/permission/page.vue
+++ src/views/permission/page.vue
@@ -1,16 +1,10 @@
 <script lang="ts" setup>
-import { useRouter } from "vue-router"
 import SwitchRoles from "./components/SwitchRoles.vue"
-
-const router = useRouter()
-const handleRolesChange = () => {
-  router.push({ path: "/403" })
-}
 </script>
 
 <template>
   <div class="app-container">
-    <SwitchRoles @change="handleRolesChange" />
+    <SwitchRoles />
     <el-tag type="warning" size="large">当前页面只有 admin 权限可见，切换权限后将不能进入该页面</el-tag>
   </div>
 </template>
