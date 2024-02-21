34276b58559967f48dd2b29c663eaa7abe45d641
pany <939630029@qq.com>
2024-02-21 13:34:50 +0800

    chore: prettier 继承 .editorconfig 的缩进风格

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,5 @@
 /**
+ * 修改配置后重启编辑器
  * 配置项文档：https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
@@ -6,8 +7,6 @@
 export default {
   /** 每一行的宽度 */
   printWidth: 120,
-  /** Tab 键的空格数 */
-  tabWidth: 2,
   /** 在对象中的括号之间是否用空格来间隔 */
   bracketSpacing: true,
   /** 箭头函数的参数无论有几个，都要括号包裹 */
@@ -19,7 +18,5 @@ export default {
   /** 对象或者数组的最后一个元素后面不要加逗号 */
   trailingComma: "none",
   /** 是否加分号 */
-  semi: false,
-  /** 是否使用 Tab 格式化 */
-  useTabs: false
+  semi: false
 }
