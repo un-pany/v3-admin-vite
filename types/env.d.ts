2ad674b0b066fe8cf8558f7c3f5c4ae35f728aed
pany <939630029@qq.com>
2024-02-22 18:52:25 +0800

    chore: remove ‘declare’

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,5 +1,5 @@
 /** 声明 vite 环境变量的类型（如果未声明则默认是 any） */
-declare interface ImportMetaEnv {
+interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string
   readonly VITE_BASE_API: string
   readonly VITE_ROUTER_HISTORY: "hash" | "html5"
