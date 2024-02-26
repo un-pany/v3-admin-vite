577a7178969a1f820dcf6d3c52803e483549aaf3
pany <panyang@mafengwo.com>
2024-02-26 22:17:33 +0800

    refactor: 简化动画

--- src/views/login/components/Owl.vue
+++ src/views/login/components/Owl.vue
@@ -31,11 +31,11 @@ const props = defineProps<Props>()
   @include backgroundImage("@/assets/login/face.png");
   .hand-down-left,
   .hand-down-right {
+    z-index: 2;
     position: absolute;
     width: 45px;
     height: 25px;
-    z-index: 1;
-    transition: 0.3s ease-out;
+    transition: transform 0.2s linear;
   }
   .hand-down-left {
     bottom: 3px;
@@ -49,58 +49,44 @@ const props = defineProps<Props>()
   }
   .hand-up-left,
   .hand-up-right {
+    z-index: 3;
     position: absolute;
     width: 50px;
     height: 40px;
-    z-index: 2;
     opacity: 0;
-    background-position: 0 25px;
-    transform-origin: 0 40px;
-    transition:
-      background-position 0.3s ease-out,
-      transform 0.3s ease-out,
-      opacity 0s linear 0.2s;
+    transition: opacity 0.1s linear 0.1s;
   }
   .hand-up-left {
-    bottom: 10px;
-    left: -3px;
-    transform: translateX(-30px) scale(0.8);
+    bottom: 11px;
+    left: -5px;
     @include backgroundImage("@/assets/login/hand-up-left.png");
   }
   .hand-up-right {
-    bottom: 10px;
+    bottom: 11px;
     right: 5px;
-    transform: translateX(55px) scale(0.8);
     @include backgroundImage("@/assets/login/hand-up-right.png");
   }
   .close-eyes {
+    z-index: 1;
     width: 100%;
     height: 100%;
     opacity: 0;
-    transition: 0.1s ease-out 0s;
+    transition: opacity 0.1s linear 0.1s;
     @include backgroundImage("@/assets/login/close-eyes.png");
   }
 }
 
 .owl-password {
   .hand-down-left {
-    transform: translateX(40px) scale(0) translateY(-10px);
+    transform: translateX(30px) scale(0) translateY(-10px);
   }
   .hand-down-right {
-    transform: translateX(-32px) scale(0) translateY(-8px);
+    transform: translateX(-40px) scale(0) translateY(-10px);
   }
   .hand-up-left,
-  .hand-up-right {
-    transform: scale(1);
-    background-position: 0 0;
-    opacity: 1;
-    transition:
-      background-position 0.3s ease-out,
-      transform 0.3s ease-out;
-  }
+  .hand-up-right,
   .close-eyes {
     opacity: 1;
-    transition: 0.1s ease-out 0.2s;
   }
 }
 </style>
