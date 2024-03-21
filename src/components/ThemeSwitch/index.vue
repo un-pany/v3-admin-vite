54e86ab517494b81b4bb2cf51d572351a194fb95
pany <panyang@mafengwo.com>
2024-03-21 09:52:54 +0800

    Revert "feat：主题切换动画"
    This reverts commit 2948f86986a75af91540ef9bfec1ec00f1a055fb.

--- src/components/ThemeSwitch/index.vue
+++ src/components/ThemeSwitch/index.vue
@@ -1,25 +1,12 @@
 <script lang="ts" setup>
-import { type ThemeName, useTheme } from "@/hooks/useTheme"
+import { useTheme } from "@/hooks/useTheme"
 import { MagicStick } from "@element-plus/icons-vue"
 
 const { themeList, activeThemeName, setTheme } = useTheme()
-
-const onChangeTheme = (event: MouseEvent, themeName: ThemeName) => {
-  document.documentElement.style.setProperty("--x", event.clientX + "px")
-  document.documentElement.style.setProperty("--y", event.clientY + "px")
-  const handler = () => setTheme(themeName)
-  // @ts-ignore
-  if (document.startViewTransition) {
-    // @ts-ignore
-    document.startViewTransition(handler)
-  } else {
-    handler()
-  }
-}
 </script>
 
 <template>
-  <el-dropdown trigger="click">
+  <el-dropdown trigger="click" @command="setTheme">
     <div>
       <el-tooltip effect="dark" content="主题模式" placement="bottom">
         <el-icon :size="20">
@@ -34,7 +21,6 @@ const onChangeTheme = (event: MouseEvent, themeName: ThemeName) => {
           :key="index"
           :disabled="activeThemeName === theme.name"
           :command="theme.name"
-          @click="(e) => onChangeTheme(e, theme.name)"
         >
           <span>{{ theme.title }}</span>
         </el-dropdown-item>
