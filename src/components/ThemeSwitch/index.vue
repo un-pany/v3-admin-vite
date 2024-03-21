c4526f72b1c59e4eabff797a99b0479c2dcd8b6e
nevlf <48950326+nevlf@users.noreply.github.com>
2024-03-21 10:00:14 +0800

    feat: 主题切换动画 (#176)
    Co-authored-by: pany <panyang@mafengwo.com>

--- src/components/ThemeSwitch/index.vue
+++ src/components/ThemeSwitch/index.vue
@@ -1,12 +1,28 @@
 <script lang="ts" setup>
-import { useTheme } from "@/hooks/useTheme"
+import { type ThemeName, useTheme } from "@/hooks/useTheme"
 import { MagicStick } from "@element-plus/icons-vue"
 
 const { themeList, activeThemeName, setTheme } = useTheme()
+
+const handleChangeTheme = ({ clientX, clientY }: MouseEvent, themeName: ThemeName) => {
+  const maxRadius = Math.hypot(
+    Math.max(clientX, window.innerWidth - clientX),
+    Math.max(clientY, window.innerHeight - clientY)
+  )
+  const style = document.documentElement.style
+  style.setProperty("--v3-theme-x", clientX + "px")
+  style.setProperty("--v3-theme-y", clientY + "px")
+  style.setProperty("--v3-theme-r", maxRadius + "px")
+  const handler = () => {
+    setTheme(themeName)
+  }
+  // @ts-expect-error
+  document.startViewTransition ? document.startViewTransition(handler) : handler()
+}
 </script>
 
 <template>
-  <el-dropdown trigger="click" @command="setTheme">
+  <el-dropdown trigger="click">
     <div>
       <el-tooltip effect="dark" content="主题模式" placement="bottom">
         <el-icon :size="20">
@@ -20,7 +36,11 @@ const { themeList, activeThemeName, setTheme } = useTheme()
           v-for="(theme, index) in themeList"
           :key="index"
           :disabled="activeThemeName === theme.name"
-          :command="theme.name"
+          @click="
+            (e) => {
+              handleChangeTheme(e, theme.name)
+            }
+          "
         >
           <span>{{ theme.title }}</span>
         </el-dropdown-item>
