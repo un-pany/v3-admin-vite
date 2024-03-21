db5422b22fb76304d5e5dd772774248d5fefae20
pany <panyang@mafengwo.com>
2024-03-21 09:57:32 +0800

    refactor: 重构

--- src/components/ThemeSwitch/index.vue
+++ src/components/ThemeSwitch/index.vue
@@ -4,17 +4,20 @@ import { MagicStick } from "@element-plus/icons-vue"
 
 const { themeList, activeThemeName, setTheme } = useTheme()
 
-const onChangeTheme = (event: MouseEvent, themeName: ThemeName) => {
-  document.documentElement.style.setProperty("--x", event.clientX + "px")
-  document.documentElement.style.setProperty("--y", event.clientY + "px")
-  const handler = () => setTheme(themeName)
-  // @ts-ignore
-  if (document.startViewTransition) {
-    // @ts-ignore
-    document.startViewTransition(handler)
-  } else {
-    handler()
+const handleChangeTheme = ({ clientX, clientY }: MouseEvent, themeName: ThemeName) => {
+  const maxRadius = Math.hypot(
+    Math.max(clientX, window.innerWidth - clientX),
+    Math.max(clientY, window.innerHeight - clientY)
+  )
+  const style = document.documentElement.style
+  style.setProperty("--v3-theme-x", clientX + "px")
+  style.setProperty("--v3-theme-y", clientY + "px")
+  style.setProperty("--v3-theme-r", maxRadius + "px")
+  const handler = () => {
+    setTheme(themeName)
   }
+  // @ts-expect-error
+  document.startViewTransition ? document.startViewTransition(handler) : handler()
 }
 </script>
 
@@ -33,8 +36,11 @@ const onChangeTheme = (event: MouseEvent, themeName: ThemeName) => {
           v-for="(theme, index) in themeList"
           :key="index"
           :disabled="activeThemeName === theme.name"
-          :command="theme.name"
-          @click="(e) => onChangeTheme(e, theme.name)"
+          @click="
+            (e) => {
+              handleChangeTheme(e, theme.name)
+            }
+          "
         >
           <span>{{ theme.title }}</span>
         </el-dropdown-item>
