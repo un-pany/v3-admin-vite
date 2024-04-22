308dc9717017d9ac241185144f8292dbfc48d8ed
pany-ang <939630029@qq.com>
2024-04-22 12:53:11 +0000

    Deploying to gh-pages from  @ 6f8755c47cdf546a166c2fd481e0db13718a8f69 🚀

---
+++ static/index-6ChvBY-Z.js
@@ -0,0 +1,1 @@
+import{i as e}from"./index-DBdOCPoM.js";function r(t){return e({url:"table",method:"post",data:t})}function u(t){return e({url:`table/${t}`,method:"delete"})}function l(t){return e({url:"table",method:"put",data:t})}function n(t){return e({url:"table",method:"get",params:t})}export{r as c,u as d,n as g,l as u};
