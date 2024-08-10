5648a5c5c48c91341c9f5f5f95246cd3dae95ae3
pany-ang <939630029@qq.com>
2024-08-10 02:29:29 +0000

    Deploying to gh-pages from  @ 372a63439b6963bc2fea951d93048f684420cc36 🚀

---
+++ static/index-Da_UzL7y.js
@@ -0,0 +1,1 @@
+import{m as t,aD as r,aE as a,p as o,q as s}from"./vue-whfKlJIt.js";const _=t({__name:"index",setup(n){const e=r();return a().replace({path:"/"+e.params.path,query:e.query}),(p,c)=>(o(),s("div"))}});export{_ as default};
