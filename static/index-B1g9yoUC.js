ccb15c6858b7194352ced3d447a5e00182621fe6
pany-ang <939630029@qq.com>
2024-10-19 02:54:02 +0000

    Deploying to gh-pages from  @ 0bcb7886d5077f9abb7d0d005c06a3b4467965b2 🚀

--- static/index-B1g9yoUC.js
+++
@@ -1,1 +0,0 @@
-import{m as t,aD as r,aE as a,p as o,q as s}from"./vue-Bp3_qCra.js";const _=t({__name:"index",setup(n){const e=r();return a().replace({path:"/"+e.params.path,query:e.query}),(p,c)=>(o(),s("div"))}});export{_ as default};
