6680e7264576d455c47d5fc47cda8c70561c4d32
pany <939630029@qq.com>
2024-11-11 19:06:19 +0800

    refactor: 重写 formatDateTime 工具函数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,4 @@
-import dayjs from "dayjs"
 import { removeConfigLayout } from "@/utils/cache/local-storage"
-
-/** 格式化时间 */
-export const formatDateTime = (time: string | number | Date) => {
-  return time ? dayjs(new Date(time)).format("YYYY-MM-DD HH:mm:ss") : "N/A"
-}
 
 /** 用 JS 获取全局 css 变量 */
 export const getCssVariableValue = (cssVariableName: string) => {
