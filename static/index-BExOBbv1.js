2283bf4aeaa5dd5d0f0a2eb28af9a490dd2e22ea
pany-ang <939630029@qq.com>
2024-11-14 09:34:56 +0000

    Deploying to gh-pages from  @ 869986bc502282ac42be39e2c461da738f0839a9 🚀

--- static/index-BExOBbv1.js
+++
@@ -1,1 +0,0 @@
-import{p as t,aD as r,aE as a,q as o,t as s}from"./vue-BG7-vYzG.js";const m=t({__name:"index",setup(n){const e=r();return a().replace({path:"/"+e.params.path,query:e.query}),(p,c)=>(o(),s("div"))}});export{m as default};
