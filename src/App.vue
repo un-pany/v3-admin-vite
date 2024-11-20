f2eba5093855cbe28f4994b779baad8c9d43acbf
pany <939630029@qq.com>
2024-11-20 15:40:24 +0800

    docs: 更新项目中的部分外链

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,7 @@ ElNotification({
   title: "Hello",
   type: "success",
   dangerouslyUseHTMLString: true,
-  message:
-    "<a style='color: teal' target='_blank' href='https://github.com/un-pany/v3-admin-vite'>小项目获取 star 不易，如果你喜欢这个项目的话，欢迎点击这里支持一个 star ！这是作者持续维护的唯一动力（小声：毕竟是免费的）</a>",
+  message: "<a style='color: teal' target='_blank' href='https://github.com/un-pany/v3-admin-vite'>小项目获取 star 不易，如果你喜欢这个项目的话，欢迎点击这里支持一个 star ！这是作者持续维护的唯一动力（小声：毕竟是免费的）</a>",
   duration: 0,
   position: "bottom-right"
 })
--- src/plugins/vxe-table/index.ts
+++ src/plugins/vxe-table/index.ts
@@ -1,8 +1,6 @@
 import type { App } from "vue"
-// https://vxetable.cn/#/table/start/install
-import VXETable from "vxe-table"
-// https://github.com/x-extends/vxe-table-plugin-element
-import VXETablePluginElement from "vxe-table-plugin-element"
+import VXETable from "vxe-table" // https://vxetable.cn/#/start/install
+import VXETablePluginElement from "vxe-table-plugin-element" // https://github.com/x-extends/vxe-table-plugin-element
 
 VXETable.use(VXETablePluginElement)
 
