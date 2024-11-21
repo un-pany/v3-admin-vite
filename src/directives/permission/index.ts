a415ade4c2d7b28eca76cb010c7bc0b58b898e8d
pany <939630029@qq.com>
2024-11-21 19:53:33 +0800

    refactor: 统一调用工具函数

--- src/directives/permission/index.ts
+++ src/directives/permission/index.ts
@@ -1,12 +1,13 @@
 import type { Directive } from "vue"
 import { useUserStore } from "@/store/modules/user"
+import { isArray } from "@/utils/validate"
 
 /** 权限指令，和权限判断函数 checkPermission 功能类似 */
 export const permission: Directive = {
   mounted(el, binding) {
     const { value: permissionRoles } = binding
     const { roles } = useUserStore()
-    if (Array.isArray(permissionRoles) && permissionRoles.length > 0) {
+    if (isArray(permissionRoles) && permissionRoles.length > 0) {
       const hasPermission = roles.some(role => permissionRoles.includes(role))
       // hasPermission || (el.style.display = "none") // 隐藏
       hasPermission || el.parentNode?.removeChild(el) // 销毁
--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -1,8 +1,9 @@
 import { useUserStore } from "@/store/modules/user"
+import { isArray } from "@/utils/validate"
 
 /** 全局权限判断函数，和权限指令 v-permission 功能类似 */
 export function checkPermission(permissionRoles: string[]): boolean {
-  if (Array.isArray(permissionRoles) && permissionRoles.length > 0) {
+  if (isArray(permissionRoles) && permissionRoles.length > 0) {
     const { roles } = useUserStore()
     return roles.some(role => permissionRoles.includes(role))
   } else {
