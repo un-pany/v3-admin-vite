24fd0a6253b343a2a97fab00b23abb5eebc4f09e
pany <939630029@qq.com>
2024-11-26 18:20:52 +0800

    perf: 扁平化 directives 目录

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,7 +1,7 @@
 import type { App } from "vue"
 import { permission } from "./permission"
 
-/** 挂载自定义指令 */
+/** 加载自定义指令 */
 export function loadDirectives(app: App) {
   app.directive("permission", permission)
 }
