503e2d8487f4d3b678b1813d7c2a13c7acdcd673
ClariS <1457715339@qq.com>
2024-12-02 10:22:18 +0800

    docs: 删除 global-components.d.ts 注释多余的括号 (#221)

--- types/global-components.d.ts
+++ types/global-components.d.ts
@@ -2,7 +2,7 @@ import type SvgIcon from "@@/components/SvgIcon/index.vue"
 
 export {}
 
-// 由 app.component 全局注册的组件需要在这里声明 TS 类型才能获得 Volar 插件提供的类型提示）
+// 由 app.component 全局注册的组件需要在这里声明 TS 类型才能获得 Volar 插件提供的类型提示
 declare module "vue" {
   export interface GlobalComponents {
     SvgIcon: typeof SvgIcon
