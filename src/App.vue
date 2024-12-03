9f8e1beafec85045a5cad32e929df0a6bfa29027
pany <939630029@qq.com>
2024-12-03 11:53:09 +0800

    chore: element plus 2.9.0

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <script lang="ts" setup>
 import { useGreyAndColorWeakness } from "@@/composables/useGreyAndColorWeakness"
+import { usePany } from "@@/composables/usePany"
 import { useTheme } from "@@/composables/useTheme"
 import zhCn from "element-plus/es/locale/lang/zh-cn" // Element Plus 中文包
 
 const { initTheme } = useTheme()
 const { initGreyAndColorWeakness } = useGreyAndColorWeakness()
+const { initStarNotification, initStoreNotification } = usePany()
 
 // 初始化主题
 initTheme()
-
 // 初始化灰色模式和色弱模式
 initGreyAndColorWeakness()
-
-// 作者小心思
-ElNotification({
-  title: "Hello",
-  type: "success",
-  dangerouslyUseHTMLString: true,
-  message: "<a style='color: teal' target='_blank' href='https://github.com/un-pany/v3-admin-vite'>小项目获取 star 不易，如果你喜欢这个项目的话，欢迎点击这里支持一个 star ！这是作者持续维护的唯一动力（小声：毕竟是免费的）</a>",
-  duration: 0,
-  position: "bottom-right"
-})
+// 初始化通知
+initStarNotification()
+initStoreNotification()
 </script>
 
 <template>
