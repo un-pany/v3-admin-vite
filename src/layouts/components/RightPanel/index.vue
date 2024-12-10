a3ec9e2ad2f3f80c6ce42994396a2c9d69b0b24d
pany <939630029@qq.com>
2024-12-10 17:36:58 +0800

    perf: 右侧设置按钮定位方式改为百分比

--- src/layouts/components/RightPanel/index.vue
+++ src/layouts/components/RightPanel/index.vue
@@ -1,16 +1,6 @@
 <script lang="ts" setup>
 import { Setting } from "@element-plus/icons-vue"
 
-interface Props {
-  buttonTop?: number
-}
-
-const props = withDefaults(defineProps<Props>(), {
-  buttonTop: 350
-})
-
-const buttonTopCss = `${props.buttonTop}px`
-
 const show = ref(false)
 </script>
 
@@ -31,7 +21,7 @@ const show = ref(false)
   height: 48px;
   background-color: var(--v3-rightpanel-button-bg-color);
   position: fixed;
-  top: v-bind(buttonTopCss);
+  top: 45%;
   right: 0;
   border-radius: 6px 0 0 6px;
   z-index: 2000;
