12f90fbeef4400744583e0872d9efe3818a253f8
pany-ang <939630029@qq.com>
2024-12-12 12:13:44 +0000

    Deploying to gh-pages from  @ 1fa9d8c1f7cc85f0d1ceacf7117f744034c048c5 🚀

---
+++ assets/el-message-box-DBB3GfP_.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index-CCNeBscs.js";function r(t){return e({url:"table",method:"post",data:t})}function u(t){return e({url:`table/${t}`,method:"delete"})}function l(t){return e({url:"table",method:"put",data:t})}function n(t){return e({url:"table",method:"get",params:t})}export{r as c,u as d,n as g,l as u};
