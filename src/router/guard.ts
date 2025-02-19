c2f5c8ee91caab0163d5180efb75fb7199c059fe
pany <939630029@qq.com>
2025-02-19 13:35:10 +0800

    refactor: 减少路由守卫中的硬编码

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -9,7 +9,10 @@ import { getToken } from "@@/utils/cache/cookies"
 import NProgress from "nprogress"
 
 NProgress.configure({ showSpinner: false })
+
 const { setTitle } = useTitle()
+
+const LOGIN_PATH = "/login"
 
 export function registerNavigationGuard(router: Router) {
   // 全局前置守卫
@@ -22,10 +25,10 @@ export function registerNavigationGuard(router: Router) {
       // 如果在免登录的白名单中，则直接进入
       if (isWhiteList(to)) return true
       // 其他没有访问权限的页面将被重定向到登录页面
-      return "/login"
+      return LOGIN_PATH
     }
     // 如果已经登录，并准备进入 Login 页面，则重定向到主页
-    if (to.path === "/login") return "/"
+    if (to.path === LOGIN_PATH) return "/"
     // 如果用户已经获得其权限角色
     if (userStore.roles.length !== 0) return true
     // 否则要重新获取权限角色
@@ -43,7 +46,7 @@ export function registerNavigationGuard(router: Router) {
       // 过程中发生任何错误，都直接重置 Token，并重定向到登录页面
       userStore.resetToken()
       ElMessage.error((error as Error).message || "路由守卫发生错误")
-      return "/login"
+      return LOGIN_PATH
     }
   })
 
